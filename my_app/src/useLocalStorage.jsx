53370306ea21b55ad9c6e4bd5783832b9a86bb21
mayur2004Id <gupta.mayur@kalvium.community>
2024-02-29 12:11:27 +0530

    hooks

--- my_app/src/useLocalStorage.jsx
+++ my_app/src/useLocalStorage.jsx
@@ -8,6 +8,7 @@ function useLocalStorage(key , initialValue) {
 
     useEffect(() => {
         localStorage.setItem(key, value);
+        sessionStorage.setItem(key, value);
     }, [key, value]);
 
     return [value, setValue];
